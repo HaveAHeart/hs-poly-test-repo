d162be51a8a270e4bfa67e02be4198a35b5bcd01
Alexey <bedrin18@mail.ru>
2023-12-27 16:26:58 +0300

    5th lesson finished

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -1,32 +1,45 @@
 module Part5.Tasks where
 
-import Util(notImplementedYet)
-
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ b [] = b
+myFoldl func initVal list = myFoldl func initVal' list' where
+  initVal' = func initVal (head list)
+  list' = tail list
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ b [] = b
+myFoldr func initVal list = myFoldr func initVal' list' where
+  initVal' = func (last list) initVal
+  list' = init list
+   
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
-
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr func [] where
+  func = \x res -> [f x] ++ res
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldr func [] where
+  func = \x res -> f x ++ res
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldr func [] where
+  func = (++)
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = foldr func [] where
+  func = \x res -> res ++ [x]
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter boolPred = foldr func [] where
+  func = \x res -> getXorEmpty x (boolPred x) ++ res
+  getXorEmpty = \x boolVal -> [x | boolVal] 
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition boolPred = foldr func ([], []) where
+  func = \x res -> (getXOrEmpty x (boolPred x) ++ fst res, getEmptyOrX x (boolPred x) ++ snd res) 
+  getXOrEmpty = \x boolVal -> [x | boolVal] 
+  getEmptyOrX = \x boolVal -> [x | not boolVal] 
 
